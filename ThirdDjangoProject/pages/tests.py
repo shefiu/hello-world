491ab579de4698a91f9af0d43bece18fa105a32f
shefiu <gshefiu@gmail.com>
2023-03-08 14:40:00 +0300

    Commit after template chapter three -Templates

--- ThirdDjangoProject/pages/tests.py
+++ ThirdDjangoProject/pages/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class HomePageTests(SimpleTestCase):
+    def test_url_exists_at_correct_locations(self):
+        response = self.client.get("/")
+        self.assertEquals(response.status_code, 200)
+        
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
+    
+    def test_template_content(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response,"<h1>Home page in Pages Application</h1>")
+    
+
+class AboutPageTests(SimpleTestCase):
+    def test_url_exists_at_correct_locations(self):
+        response = self.client.get("/about/")
+        self.assertEquals(response.status_code, 200) 
+        
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("about"))
+        self.assertTemplateUsed(response, "about.html")
+    
+    def test_template_content(self):
+        response = self.client.get(reverse("about"))
+        self.assertContains(response,"<h1>About Page</h1>")
